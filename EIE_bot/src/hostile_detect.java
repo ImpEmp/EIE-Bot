901eecd02c0a4296a6f5fee863ee38507daa9039
Cameron Jenkins <cameronjenkins@10.0.1.107>
2017-06-24 18:47:44 -0400

    ada

--- EIE_bot/src/hostile_detect.java
+++ EIE_bot/src/hostile_detect.java
@@ -1,4 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class hostile_detect {
+public Boolean twitterban(String text){
+	
+	return null;
+	
+}
+public Boolean is_tagged(String text){
+	
+	return null;
+	
+}
+public int Cursewords(String s){
+int y = 0;
+	 String[] arr = s.split(" ");    
 
+	 for (String ss : arr) {
+		 File file = new File("bad.txt");
+
+		 try {
+		     @SuppressWarnings("resource")
+			Scanner scanner = new Scanner(file);
+
+		     //now read the file line by line...
+
+		     while (scanner.hasNextLine()) {
+		         String line = scanner.nextLine();
+		         if(ss==line) { 
+		           y++;  
+		         }
+		     }
+		 } catch(FileNotFoundException e) { 
+		     //handle this
+		 }
+	  }
+
+	return y;
+	
+}
+public Boolean facebookban(String text){
+	return null;
+	
+}
+public Boolean generalban(String text){
+	return null;
+}
 }
