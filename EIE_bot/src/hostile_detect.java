e4fb75ee8c6b118b035fdb7c055e12afe7b01d94
Cameron Jenkins <cameronjenkins@10.0.1.107>
2017-06-25 14:10:00 -0400

    stuff

--- EIE_bot/src/hostile_detect.java
+++ EIE_bot/src/hostile_detect.java
@@ -18,53 +18,9 @@ public Boolean is_tagged(String text){
 	return null;
 	
 }
-static int largestWordLength = 0;
-public static void loadConfigs() {
-    try {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("https://docs.google.com/spreadsheets/d/1hIEi2YG3ydav1E06Bzf2mQbGZ12kh2fe4ISgLg_UBuM/export?format=csv").openConnection().getInputStream()));
-        String line = "";
-        int counter = 0;
-        while((line = reader.readLine()) != null) {
-            counter++;
-            String[] content = null;
-            try {
-                content = line.split(" ");
-                if(content.length == 0) {
-                    continue;
-                }
-                String word = content[0];
-                String[] ignore_in_combination_with_words = new String[]{};
-                if(content.length > 1) {
-                    ignore_in_combination_with_words = content[1].split("_");
-                }
 
-                if(word.length() > largestWordLength) {
-                    largestWordLength = word.length();
-                }
-             //   words.put(word.replaceAll(" ", ""), ignore_in_combination_with_words);
-
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-        System.out.println("Loaded " + counter + " words to filter out");
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-}
-
-
-/**
- * Iterates over a String input and checks whether a cuss word was found in a list, then checks if the word should be ignored (e.g. bass contains the word *ss).
- * @param input
- * @return
- */
-public static ArrayList<String> badWordsFound(String input) {
-    if(input == null) {
-        return new ArrayList<>();
-    }
+public static String leetspeekremover(String input) {
+ 
 
     // remove leetspeak
     input = input.replaceAll("1","i");
@@ -76,48 +32,20 @@ public static ArrayList<String> badWordsFound(String input) {
     input = input.replaceAll("7","t");
     input = input.replaceAll("0","o");
     input = input.replaceAll("9","g");
-
-    ArrayList<String> badWords = new ArrayList<>();
-    input = input.toLowerCase().replaceAll("[^a-zA-Z]", "");
-
-    // iterate over each letter in the word
-    for(int start = 0; start < input.length(); start++) {
-        // from each letter, keep going to find bad words until either the end of the sentence is reached, or the max word length is reached. 
-        for(int offset = 1; offset < (input.length()+1 - start) && offset < largestWordLength; offset++)  {
-            String wordToCheck = input.substring(start, start + offset);
-         if(badWords.contains(wordToCheck)) {
-                // for example, if you want to say the word bass, that should be possible.
-//                String[] ignoreCheck = words.get(wordToCheck);
-               boolean ignore = false;
-//                for(int s = 0; s < ignoreCheck.length; s++ ) {
-//                    if(input.contains(ignoreCheck[s])) {
-//                        ignore = true;
-//                        break;
-//                    }
-//                }
-        	 
-                if(!ignore) {
-                    badWords.add(wordToCheck);
-                }
-            }
-        }
+return input;
     }
 
 
-    for(String s: badWords) {
-        System.out.print(s + " qualified as a bad word in a username");
-    }
-    return badWords;
-
-}
+ 
 
 
-public static String filterText(String input, String username) {
-    ArrayList<String> badWords = badWordsFound(input);
-    if(badWords.size() > 0) {
+public static String filterText(String input) {
+	input = leetspeekremover(input);
+	Boolean bad = true;
+    if(bad == true)
         return "This message was blocked because a bad word was found. If you believe this word should not be blocked, please message support.";
-    }
-    return input;
+    
+    return " ";
 }
 public Boolean facebookban(String text){
 	return null;
--- EIE_bot/src/tester.java
+++ EIE_bot/src/tester.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
+
 
 public class tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		hostile_detect d = new hostile_detect();
-		int y =  d.Cursewords("ass fag faggot");
-		System.out.print(y);
+		hostile_detect d = new hostile_detect ();
+		System.out.print("y");
 	}
 
 }
